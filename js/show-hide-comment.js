ec1be24f62e42d9eb74555faacf784b6f96af0a3
iwataj <iwata@sansan.com>
2016-12-16 20:09:56 +0900

    Fix: Follow GitHub implementation changes in "Show/Hide All Comments"

--- js/show-hide-comment.js
+++ js/show-hide-comment.js
@@ -1,23 +1,23 @@
 $(document).bind("ghp:ready", function () {
-    if ($(".outdated-diff-comment-container").length) {
+    if ($(".outdated-comment").length) {
         var unfoldIcon = $("svg.octicon-unfold").prop("outerHTML");
         var foldIcon = $("svg.octicon-fold").prop("outerHTML");
 
-        var button = '<div class="discussion-item outdated-diff-comment-container">'
-            + '<div class="discussion-item-header">'
-            + '<span class="discussion-item-icon discussion-item-toggle-closed">' + unfoldIcon + '</span>'
-            + '<span class="discussion-item-icon discussion-item-toggle-open">' + foldIcon + '</span>'
-            + '<button type="button" class="btn-link discussion-item-toggle discussion-item-toggle-closed js-show-hide-comment">'
-            + unfoldIcon + ' Show all comments</button>'
-            + '<button type="button" class="btn-link discussion-item-toggle discussion-item-toggle-open js-show-hide-comment">'
-            + foldIcon + ' Hide all comments</button>'
+        var button = '<div class="discussion-item discussion-item-body">'
+            + '<div class="discussion-item-header outdated-comment">'
+            + '<span class="discussion-item-icon show-outdated-button" style="padding-left: 4px">' + unfoldIcon + '</span>'
+            + '<span class="discussion-item-icon hide-outdated-button" style="padding-left: 4px">' + foldIcon + '</span>'
+            + '<button type="button" class="btn btn-sm btn-outline outdated-comment-label show-outdated-button js-show-hide-comment">'
+            + unfoldIcon + ' Show all outdated</button>'
+            + '<button type="button" class="btn btn-sm btn-outline outdated-comment-label hide-outdated-button js-show-hide-comment">'
+            + foldIcon + ' Hide all outdated</button>'
             + '</div></div>';
 
         $(".pull-discussion-timeline .timeline-comment-wrapper:first").after(button);
 
         $(".js-show-hide-comment").click(function () {
-            var toOpen = $(this).hasClass("discussion-item-toggle-closed");
-            $(".outdated-diff-comment-container").toggleClass("open", toOpen);
+            var toOpen = $(this).hasClass("show-outdated-button");
+            $(".outdated-comment").toggleClass("open", toOpen);
         });
     }
 });
